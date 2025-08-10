27d1af9e42e58dff287b452c18c07a53fd9e1b61
Irshad7.eng <mdiayan777@gmail.com>
2025-08-10 18:36:00 +0530

    RestAPI and middlewere concept

---
+++ restApi/index.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const users = require("./MOCK_DATA.json");
+const  app = express();
+const fs = require('fs');
+const { error } = require('console');
+
+const PORT = 8000;
+
+// middleware for post request
+
+app.use(express.urlencoded({extended: false}));
+
+app.use((req,res, next) =>{
+    console.log("i am from middleware 1");
+    next();
+});
+app.use((req, res, next) =>
+{
+    fs.appendFile('tast.txt', `${Date.now()}: ${req.method}:${req.path}`, (error, data) =>{
+       next();
+    });
+   
+})
+
+//  GET Routers
+app.get('/api/users', (req, res) =>
+{
+    return res.json(users);
+});
+
+app.get('/users', (req, res) =>
+{
+    const html = `
+    <ul>
+    ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+    </ul>
+    `;
+    res.send(html);
+})
+
+// get put patch, delete user by id
+
+app.route("/api/users/:id")
+.get((req, res) =>
+{
+    const id = Number(req.params.id);
+
+    const user = users.find((user) => user.id === id);
+
+    return res.json(user);
+})
+.put((req, res) => {
+    return res.json({ status:"panding"});
+})
+.patch((req, res) => {
+    return res.json({ status:"panding"});
+})
+.delete((req, res) => {
+    return res.json({ status:"panding"});
+});
+
+// post request create new user
+app.post('/api/users', (req, res) => {
+    const body = req.body;
+    users.push({...body, id: users.length+1});
+    fs.writeFile('./MOCK_DATA.json',JSON.stringify(users), (err, data) =>{
+       return res.json({ status:"sucess", id: users.length}); 
+    });
+    
+});
+
+
+
+app.listen(PORT, ()=>{console.log("server is start")});
